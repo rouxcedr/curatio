d302e5f9fcde209b85fc41fcd496634c62b41a5d
rouxcedr <cedric.roux22@gmail.com>
2020-11-04 16:09:30 -0500

    Project Start

---
+++ eauxbelix/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EauxbelixConfig(AppConfig):
+    name = 'eauxbelix'
---
+++ formations/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FormationsConfig(AppConfig):
+    name = 'formations'
